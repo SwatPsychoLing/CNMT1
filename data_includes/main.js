8c666fe010ad7e76cc35e7f728dd2bfb3cd30cae
eorticio <eorticio@gmail.com>
2019-10-06 15:30:20 -0400

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -37,63 +37,6 @@ PennController( "intro" ,
 
 PennController.CheckPreloaded("experiment").label("preload")
 
-
-PennController.Template( 
-  variable => PennController( "experiment" ,
-    newButton("Go")
-        .print( "center at 50vw" , "center at 90vh" )
-        .wait()
-        .remove()
-    ,
-    newMouseTracker("mouse")
-        .settings.log()
-        .start()
-    ,
-    newImage("1", variable.Image1)
-        .settings.size(200,200)
-    ,
-    newImage("2", variable.Image2)
-        .settings.size(200,200)
-    ,
-    newImage("3", variable.Image3)
-        .settings.size(200,200)
-    ,
-    newImage("4", variable.Image4)
-        .settings.size(200,200)
-    ,
-    newCanvas("images", 1400, 200)
-        .settings.center()
-        .settings.add(0   , 0 , getImage("1") )
-        .settings.add(200 , 0 , getImage("2") )
-        .settings.add(1000 , 0 , getImage("3") )
-        .settings.add(1200, 0 , getImage("4") )
-        .print()
-    ,
-    newTimer(1200)
-        .start()
-        .wait()
-    ,
-    newAudio("description", variable.AudioFile)
-        .play()
-    ,
-    newSelector()
-        .settings.add( getImage("1") , getImage("2") , getImage("3") , getImage("4"))
-        .settings.log()
-        .wait()
-    ,
-    getMouseTracker("mouse")
-        .stop()
-    //,
-    //getAudio("description")
-    //   .wait("first")
-  )
-  .log( "ID"     , getVar("ID")    )
-  .log( "Target"   , variable.TargetLocation  )
-  .log( "TrialType" , variable.TrialType )
-)
-
-["sep", "Separator", {transfer: 1000, normalMessage: "You have completed the main experiment. Now, please answer the following questions."}]
-
 PennController( "questions" ,
     newDropDown("mouse", "Select")
         .settings.add( "mouse" , "trackpad", "other") 
@@ -184,4 +127,58 @@ PennController( "final" ,
     ,
     newButton("void")
         .wait()
+)
+
+PennController.Template( 
+  variable => PennController( "experiment" ,
+    newButton("Go")
+        .print( "center at 50vw" , "center at 90vh" )
+        .wait()
+        .remove()
+    ,
+    newMouseTracker("mouse")
+        .settings.log()
+        .start()
+    ,
+    newImage("1", variable.Image1)
+        .settings.size(200,200)
+    ,
+    newImage("2", variable.Image2)
+        .settings.size(200,200)
+    ,
+    newImage("3", variable.Image3)
+        .settings.size(200,200)
+    ,
+    newImage("4", variable.Image4)
+        .settings.size(200,200)
+    ,
+    newCanvas("images", 1400, 200)
+        .settings.center()
+        .settings.add(0   , 0 , getImage("1") )
+        .settings.add(200 , 0 , getImage("2") )
+        .settings.add(1000 , 0 , getImage("3") )
+        .settings.add(1200, 0 , getImage("4") )
+        .print()
+    ,
+    newTimer(1200)
+        .start()
+        .wait()
+    ,
+    newAudio("description", variable.AudioFile)
+        .play()
+    ,
+    newSelector()
+        .settings.add( getImage("1") , getImage("2") , getImage("3") , getImage("4"))
+        .settings.log()
+        .wait()
+    ,
+    getMouseTracker("mouse")
+        .stop()
+    //,
+    //getAudio("description")
+    //   .wait("first")
+  )
+  .log( "ID"     , getVar("ID")    )
+  .log( "Target"   , variable.TargetLocation  )
+  .log( "TrialType" , variable.TrialType )
 )
